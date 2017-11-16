addcd3435b7dffb0e089f8485cda6a0c3754bf5f
arko-loves-to-code <arkajyoti31@gmail.com>
2017-11-16 19:21:52 +0530

    bishmay burher changes

---
+++ src/app/components/user/user.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class UserComponent implements OnInit {
+  name:string;
+  age:number;
+  email:string;
+  address:Address;
+  hobbies:string[];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.name = 'Bishmay';
+    this.age = 80;
+    this.email = 'bhismayEatsBurger@bhaghyashree.com'
+    this.address = {
+      room: 391,
+      college: 'KIIT',
+      city: 'BhaghyaLand'
+    }
+    this.hobbies = ['Eats Burger', 'Eats some more burger', 'Becomes burger king'];
+  }
+
+}
+
+interface Address{
+  room:number,
+  college:string,
+  city:string
+}
